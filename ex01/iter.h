50b878f022cd01d8676c2c7ee946ad74c034f2cb
Felipe <josfelip@debian12>
2025-02-14 18:53:03 -0300

    Refactor iter.h to include const versions of the iter function

---
+++ ex01/iter.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 18:15:43 by josfelip          #+#    #+#             */
+/*   Updated: 2025/02/14 18:15:46 by josfelip         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <cstddef>
+
+template<typename T>
+void	iter(T *array, size_t length, void (*f)(T &))
+{
+	if (!array || !f)
+		return ;
+	for (size_t i = 0; i < length; i++)
+		f(array[i]);
+}
+
+template<typename T>
+void	iter(const T *array, size_t length, void (*f)(const T &))
+{
+	if (!array || !f)
+		return ;
+	for (size_t i = 0; i < length; i++)
+		f(array[i]);
+}
+
+#endif
